0334fa2dfbb67b9148274bf716583a9056b1d39d
yanchiiantso <yanchiian0327@gmail.com>
2024-11-27 04:00:59 +0800

    Update basegame.js

--- public/scripts/basegame.js
+++ public/scripts/basegame.js
@@ -1,15 +1,17 @@
 // New file.
 
-// global variables
+// set up canvas
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+
+// other global variables
 let gameAnimFrameId = null; // track requestAnimationFrame's id, in case we need to cancel the callback
 let restoreJoinButtonTimeout = null;
-
-// this is global too, in case console throws error accessing this variable from socket
-let returnData = null;
+let returnData = null; // this is global too, in case console throws error accessing this variable from socket
 
 // modules
 
-// if timer is already present, ignore this
+// timer is already there, ignore this
 const timerSprite = function(ctx, x, y) {
 	// x and y are in the middle of the timer object
 	const draw = function(time, colour = "#000") {
@@ -27,7 +29,7 @@ const timerSprite = function(ctx, x, y) {
 	return { draw };
 };
 
-// if health bars are already present, ignore this
+// health bars are already there, ignore this
 const healthBar = function(ctx, x, y, w, h, side) {
 	// x and y are the top-left corner of the bar
 	// side records which side of the screen the health bar refers to
@@ -59,46 +61,20 @@ const healthBar = function(ctx, x, y, w, h, side) {
 	return { draw };
 };
 
-// functions
-function drawBackground(cv, ctx, unseenBorders, gap) {
-	// this draws all the stuff that shouldn't be changed during gameplay (e.g. flavour text)
-	// if something similar is present ignore this
-	ctx.save();
-	ctx.fillStyle = "#999";
-	ctx.fillRect(0, 0, cv.width, unseenBorders.top - gap.top);
-	ctx.textAlign = "center";
-	ctx.textBaseline = "middle";
-	ctx.font = "40px Arial, Helvetica, sans-serif";
-	ctx.fillStyle = "#000";
-	ctx.fillText("TIME", 640, 30);
-	ctx.font = "20px Arial, Helvetica, sans-serif";
-	ctx.textAlign = "right";
-	ctx.fillText("HP", 50, 90);
-	ctx.textAlign = "left";
-	ctx.fillText("HP", 1230, 90);
-	const groundGradient = ctx.createLinearGradient(0, unseenBorders.bottom + gap.bottom, 0, cv.height);
-	groundGradient.addColorStop(0, "white");
-	groundGradient.addColorStop(0.4, "lime");
-	groundGradient.addColorStop(0.8, "green");
-	groundGradient.addColorStop(1, "#040");
-	ctx.fillStyle = groundGradient;
-	ctx.fillRect(0, unseenBorders.bottom + gap.bottom, cv.width, cv.height);
-	ctx.restore();
-};
-
-function stopGame() { // called when opponent disconnected, this should stop the game preemptively and restore the main page
+// stopGame: called when opponent disconnects, this should stop the game preemptively and restore the main page
+function stopGame() {
 	// stop game animations (if game already started)
 	if (gameAnimFrameId) {
 		cancelAnimationFrame(gameAnimFrameId);
 		gameAnimFrameId = null;
 	};
-	// hide the game canvas
-	document.querySelector("canvas").style.display = "none";
+	// hide the game area
+	document.getElementById("game_area").style.display = "none";
 	// prepare the message on the main page
 	document.getElementById("waitingtext").innerHTML = "Unfortunately, your opponent disconnected! This game will not count towards your records.";
 	// show the main page
 	document.getElementById("main_page").style.display = "block";
-	// allow the player to join another game after a delay
+	// allow the player to join another game after a delay (2 seconds)
 	restoreJoinButtonTimeout = setTimeout(restoreJoinButton, 2000);
 };
 
@@ -106,117 +82,172 @@ function stopGame() { // called when opponent disconnected, this should stop the
 const initialiseGame = function(side) {
 	// hide all things related to the main page
 	document.getElementById("main_page").style.display = "none";
-	// show the game canvas
-	document.querySelector("canvas").style.display = "block";
+	// show the game area
+	document.getElementById("game_area").style.display = "inline-block";
 	
-	// canvas reference and context
-	const cv = document.querySelector("canvas");
-	const ctx = cv.getContext("2d");
-	
-	// variables (some of these can be ignored / replaced)
-	let gameTime = 150; // time remaining, in seconds
-	const maxGameTime = gameTime; // total duration, in seconds
-	let gameStartTime = 0;
-	const maxHP = 100; // max HP
-	const unseenBorders = { top: 140, bottom: 540 };
-	const gap = { top: 20, bottom: 40 };
+	// variables
+	const gravity = 0.7;
+	timer = 90; // time remaining, in seconds
 	const otherside = side == "left" ? "right" : "left";
-	
-	// game objects (same)
-	const gameArea = BoundingBox(ctx, unseenBorders.top, 40, unseenBorders.bottom, 1240);
-	const timer = timerSprite(ctx, 640, 90);
-	const HPbars = {
-		left: healthBar(ctx, 60, 70, 480, 40, "left"),
-		right: healthBar(ctx, 740, 70, 480, 40, "right"),
+	let lastkey; // there's only one player, and it might be either, so I'm using a variable outside the sprite
+	const attackFrame = { left: 4, right: 2 } // the frame the fighters register their attack at are different
+	const keys = {
+		ArrowRight: { pressed: false },
+		ArrowLeft: { pressed: false }
 	};
+	
+	// game objects
+	const background = new Sprite({
+		position: { x: 0, y: 0 },
+		imageSrc: "img/background.jpg"
+	});
 	const players = {
-		left: Player(ctx, 240, 360, gameArea, "left", maxHP),
-		right: Player(ctx, 1040, 360, gameArea, "right", maxHP),
+		left: new Fighter({
+			position: { x: 0, y: 0 },
+			velocity: { x: 0, y: 0 },
+			imageSrc: "img/man/Idle.png",
+			framesMax: 10,
+			scale: 3,
+			offset: { x: 0, y: 125 },
+			sprites: {
+				idle: { imageSrc: "img/man/Idle.png", framesMax: 10 },
+				run: { imageSrc: "img/man/Run.png", framesMax: 8 },
+				jump: { imageSrc: "img/man/Jump.png", framesMax: 3 },
+				fall: { imageSrc: "img/man/Fall.png", framesMax: 3 },
+				attack1: { imageSrc: "img/man/Attack1.png", framesMax: 7 },
+				takeHit: { imageSrc: "img/man/TakeHit.png", framesMax: 3 },
+				death: { imageSrc: "img/man/Death.png", framesMax: 11 },
+			},
+			attackBox: {
+				offset: { x: 130, y: 50 },
+				width: 160,
+				height: 50
+			}
+		}),
+		right: new Fighter({
+			position: { x: 700, y: 0 },
+			velocity: { x: 0, y: 0 },
+			color: "blue",
+			imageSrc: "img/woman/Idle.png",
+			framesMax: 8,
+			scale: 3.2,
+			offset: { x: 120, y: 189 },
+			sprites: {
+				idle: { imageSrc: "img/woman/Idle.png", framesMax: 8 },
+				run: { imageSrc: "img/woman/Run.png", framesMax: 8 },
+				jump: { imageSrc: "img/woman/Jump.png", framesMax: 2 },
+				fall: { imageSrc: "img/woman/Fall.png", framesMax: 2 },
+				attack1: { imageSrc: "img/woman/Attack1.png", framesMax: 5 },
+				takeHit: { imageSrc: "img/woman/Take hit.png", framesMax: 3 },
+				death: { imageSrc: "img/woman/Death.png", framesMax: 8 },
+			},
+			attackBox: {
+				offset: { x: -220, y: 100 },
+				width: 170,
+				height: 50
+			}
+		})
 	};
 	
 	// handle key presses
 	const handleKeydown = function(e) {
 		switch(e.key) {
-			case "ArrowUp": players[side].jump(); break;
-			case "ArrowLeft": players[side].setAcc(-3, 0); break;
-			case "ArrowRight": players[side].setAcc(3, 0); break;
-			case "c": // cheat mode key (can change)
-			case "C": if (!e.repeat) players[side].toggleCheating(); // only trigger once!
+			case "ArrowUp": players[side].velocity.y = -20; break;
+			case "ArrowLeft": keys.ArrowLeft.pressed = true; lastkey = "ArrowLeft"; break;
+			case "ArrowRight": keys.ArrowRight.pressed = true; lastkey = "ArrowRight"; break;
+			case " ": players[side].attack(); break;
+			// cheat key is toggleable (if cheat key is to be pressed continuously, then replace the if-condition + statement in the next line with "players[side].cheat = true" and add "players[side].cheat = false; break;" to case (cheatkey) of handleKeyup)
+			case "t": if (!e.repeat) players[side].cheat = !players[side].cheat; break; // only trigger once! (e.repeat is true if the key is continuously pressed)
 			default: break;
 		};
 	};
 	const handleKeyup = function(e) {
 		switch(e.key) {
-			case "ArrowLeft":
-			case "ArrowRight": players[side].setAcc(0, 0); break;
+			case "ArrowLeft": keys.ArrowLeft.pressed = true; break;
+			case "ArrowRight": keys.ArrowRight.pressed = false; break;
 			default: break;
 		}
 	};
-	
-	const endGame = function() { // called when some player's HP reaches 0, or if time is up
+
+	// endGame: called when some player's HP reaches 0, or if time is up
+	const endGame = function() {
 		// not yet implemented
 	};
 	
-	// gameFrame
-	const gameFrame = function(now) {
-		// update game time
-		if (gameStartTime > 0) {
-			gameTime = Math.ceil((maxGameTime * 1000 + gameStartTime - now) / 1000);
-		} else gameStartTime = now;
+	// gameFrame: called every frame
+	const gameFrame = function() {		
+		// clear the screen, then draw the game objects and the background
+		c.fillStyle = "black";
+		c.fillRect(0, 0, canvas.width, canvas.height);
+		background.update();
+		c.fillStyle = "rgba(255, 255, 255, 0.15)";
+		c.fillRect(0, 0, canvas.width, canvas.height);
+		players[side].update();
+		players[otherside].update();
+
+		// movement, but only for one player
+		players[side].velocity.x = 0;
+		if (keys.ArrowRight.pressed && lastkey === "ArrowRight") {
+			players[side].velocity.x = 5;
+			players[side].switchSprite("run");
+		} else if (keys.ArrowLeft.pressed && lastkey === "ArrowLeft") {
+			players[side].velocity.x = -5;
+			players[side].switchSprite("run");
+		} else {
+			players[side].switchSprite("idle");
+		}
+		if (players[side].velocity.y < 0) {
+			players[side].switchSprite("jump");
+		} else if (players[side].velocity.y > 0) {
+			players[side].switchSprite("fall");
+		}
+
+		// collision detection, but only for one player
+		if (rectangularCollision({ rectangle1: players[side], rectangle2: players[otherside] })
+			&& players[side].isAttacking && players[side].framesCurrent === attackFrame[side] {
+			players[otherside].takeHit();
+			players[side].isAttacking = false;
+			// this is (almost) the only part where the two player scripts have to be separated
+			if (side == "left") {
+				gsap.to("#player2Health", { width: players.right.health + "%" });
+			} else {
+				gsap.to("#player1Health", { width: players.left.health + "%" });
+			}
+		}
+		if (players[side].isAttacking && players[side].framesCurrent === attackFrame[side] {
+			players[side].isAttacking = false;
+		}
 		
-		// update this player
-		players[side].update(now);
-		
-		// prepare data to send to server (can add more data inputs if needed)
+		// prepare data to send to server
 		let data = {};
-		data.pos = players[side].getPos();
-		data.vel = players[side].getVel();
-		data.HP = players[side].getHP();
 		
 		// send to server
 		Socket.updateOtherPlayer(JSON.stringify(data));
 		
-		// update other player using data from server
-		players[otherside].updateFromServer(now, returnData);
-		
-		// clear the screen, then draw the game objects and the background
-		ctx.clearRect(0, 0, cv.width, cv.height);
-		drawBackground(cv, ctx, unseenBorders, gap);
-		if (gameTime < 11) {
-			timer.draw(gameTime, "#f00");
-		} else if (gameTime < 31) {
-			timer.draw(gameTime, "#c40");
-		} else {
-			timer.draw(gameTime);
+		// update other player using data from server (and some attributes of this player too)
+		if (returnData) {
+			players[otherside].
 		}
-		HPbars.left.draw(players.left.getHP(), maxHP);
-		HPbars.right.draw(players.right.getHP(), maxHP);
-		players.left.draw();
-		players.right.draw();
 		
 		// end game detection
-		if (gameTime < 1 || players.left.getHP() < 1 || players.right.getHP() < 1) {
+		if (players.left.health <= 0 || players.right.health <= 0) {
 			gameAnimFrameId = null;
 			removeEventListener("keydown", handleKeydown);
 			removeEventListener("keyup", handleKeyup);
 			returnData = {};
+			determineWinner({ players[left], players[right], timerId });
 			endGame();
 		} else {
 			gameAnimFrameId = requestAnimationFrame(gameFrame);
 		};
 	};
 	
-	// game canvas initialisation
-	drawBackground(cv, ctx, unseenBorders, gap);
-	timer.draw(gameTime);
-	HPbars.left.draw(maxHP, maxHP);
-	HPbars.right.draw(maxHP, maxHP);
-	players.left.draw();
-	players.right.draw();
-	
 	// event listener for keys
 	addEventListener("keydown", handleKeydown);
 	addEventListener("keyup", handleKeyup);
+
+	// start the timer
+	decreaseTimer();
 	
 	gameAnimFrameId = requestAnimationFrame(gameFrame);
 };
